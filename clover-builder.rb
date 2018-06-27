2996b17fe9d3b5dd691d29ec7a97e9ceb1185b07
Pauli Jokela <pauli.jokela@didstopia.com>
2018-06-27 10:21:47 +0300

    Fixing formula

--- clover-builder.rb
+++ clover-builder.rb
@@ -34,7 +34,7 @@ class CloverBuilder < Formula
     #Dir.chdir buildpath/"go/src/github.com/Dids/clover-builder-cli" do
     Dir.chdir(buildpath/"go/src/github.com/Dids/clover-builder-cli")
     system "pwd"
-    system "go", "get", "."
+    system "cd go/src/github.com/Dids/clover-builder-cli", "go", "get", "."
     Dir.chdir(buildpath)
     #end
 
