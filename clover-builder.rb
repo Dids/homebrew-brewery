7dd3d72353d6fd4fa20a68edc2f612e4fcf9e43c
Pauli Jokela <pauli.jokela@didstopia.com>
2018-06-27 09:45:16 +0300

    Initial commit

---
+++ clover-builder.rb
@@ -0,0 +1,49 @@
+class CloverBuilder < Formula
+  desc "A command line application for building Clover"
+  homepage "https://github.com/Dids/clover-builder-cli"
+  url "https://github.com/Dids/clover-builder-cli/archive/987867fa9c4d8455a43eed2df1cd27525ac29368.tar.gz"
+  sha256 "f146077e881b1812bff61cf67aa315165c5456e131919dc233bcd198145c57d8"
+  version "0.0.1"
+
+  ## TODO: We technically require build tools too, so Xcode CLI tools should be required?
+
+  # We need go for building
+  depends_on "go" => :build
+
+  # The rest are required for running
+  depends_on "git"
+  depends_on "svn"
+
+  ## TODO: What is this?
+  bottle :unneeded
+
+  def install
+    # Define GOPATH
+    ENV["GOPATH"] = buildpath
+    
+    # Create the parent directory
+    (buildpath/"src/github.com/Dids").mkpath
+
+    # Symlink the package directory
+    ln_s buildpath, buildpath/"src/github.com/Dids/clover-builder-cli"
+    
+    #system "cd", buildpath/"src/github.com/Dids/clover-builder-cli"
+    #system "go", "get", "./"
+    #system "cd", "-"
+
+    # Install build dependencies
+    Dir.chdir buildpath/"src/github.com/Dids/clover-builder-cli" do
+      system "go", "get", "./"
+    end
+
+    # Build the application
+    system "go", "build", "-o", bin/"clover-builder"
+    #bin.install ".scripts/build.sh"
+  end
+
+  ## TODO: Properly test and implement this
+  test do
+    ## TODO: Also run go tests? Maybe? No?
+    system bin/"clover-builder", "--help"
+  end
+end
