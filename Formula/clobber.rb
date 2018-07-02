268e9845b7aa8ee01c55e8bf71639f4ec366fb17
Pauli Jokela <pauli.jokela@didstopia.com>
2018-07-02 12:50:50 +0300

    Tweaking Clobber formula

--- Formula/clobber.rb
+++ Formula/clobber.rb
@@ -4,6 +4,7 @@ class Clobber < Formula
   url "https://github.com/Dids/clobber/archive/c0e0370aac151f068f62baa977d27551b7ca03b2.tar.gz"
   sha256 "bcdfa31d986960afe497b148e31596dd00045c1fe35db8fc43b9bb803ab8999f"
   #version "0.0.1"
+  #revision 0
 
   # Setup head/master branch support (install with --HEAD)
   head "https://github.com/Dids/Clobber.git"
@@ -17,7 +18,7 @@ class Clobber < Formula
   depends_on "go" => :build
   
   # We need Xcode/xcodebuild for running the application
-  depends_on :xcode => :because => "lorem ipsum"
+  depends_on :xcode
 
   # The rest are required for running
   depends_on "subversion"
