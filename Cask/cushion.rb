3ed1cda7b1db711c875710330a4184aaf499c3a7
Pauli Jokela <pauli.jokela@didstopia.com>
2018-09-13 11:09:41 +0300

    Added Cushion.app cask

---
+++ Cask/cushion.rb
@@ -0,0 +1,10 @@
+cask 'cushion' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://cushionapp.com/uploads/cushion-menubar.zip'
+  name 'Cushion'
+  homepage 'https://my.cushionapp.com/add-ons/macos-menubar-app'
+
+  app 'Cushion.app'
+end
