cb0e6d386d33bd1175d537c0321f8b643c90cc7a
Pauli Jokela <pauli.jokela@didstopia.com>
2018-12-11 11:36:29 +0200

    Fixed Clobber formula

--- Formula/clobber.rb
+++ Formula/clobber.rb
@@ -5,8 +5,8 @@ class Clobber < Formula
   sha256 "6ed7a518f0bc2b67e84c828c61167f6bd93bb0739d07d09cfe6524084bf5bb88"
   revision 0
 
-  # Setup head/master branch support (install with --HEAD)
-  head "https://github.com/Dids/Clobber.git"
+  # Setup HEAD support (install with --HEAD)
+  head "https://github.com/Dids/Clobber.git", :branch => "master"
 
   # No bottling necessary
   bottle :unneeded
@@ -23,7 +23,6 @@ class Clobber < Formula
   # We need Xcode/xcodebuild for running the application
   depends_on :xcode
 
-  ## FIXME: There MUST be a better way to handle "virtual" Go folder structures..
   # Define the installation steps
   def install
     # Define GOPATH
@@ -32,37 +31,24 @@ class Clobber < Formula
     # Create the required directory structure
     (buildpath/"go/bin").mkpath
     (buildpath/"go/pkg").mkpath
-    #(buildpath/"go/src").mkpath
-    (buildpath/"go/src/github.com/Dids/clobber").mkpath
+    (buildpath/"go/src").mkpath
 
-    # Copy everything to the Go project directory (except the go/ folder)
-    system "rsync -a ./ go/src/github.com/Dids/clobber/ --exclude=go/"
+    # Print out target version
+    ohai "Building version #{version}.."
 
-    # Switch to the Go project directory
-    Dir.chdir 'go/src/github.com/Dids/clobber' do
-      ohai "Switched to directory: #{Dir.pwd}"
-      
-      # Install build dependencies
-      system "dep ensure"
+    # Build the application
+    system "make", "deps"
+    system "make", "build", "test", "BINARY_VERSION=#{version}"
 
-      # Print out target version
-      ohai "Building version #{version}.."
+    # Print the version
+    system buildpath/"clobber", "--version"
 
-      ## NOTE: Homebrew/Formula/Ruby doesn't seem to like escaping quotes,
-      ##       so we had to resort to using a build script/wrapper instead
-      # Build the application
-      system "./.scripts/build.sh", version, buildpath/"clobber"
+    # Install the application
+    bin.install buildpath/"clobber"
 
-      # Print the version
-      system buildpath/"clobber",  "--version"
-
-      # Install the application
-      bin.install buildpath/"clobber"
-
-      # Test that the version matches
-      if "clobber version #{version}" != `#{bin}/clobber --version`.strip
-        odie "Output of 'clobber --version' did not match the current version (#{version})."
-      end
+    # Test that the version matches
+    if "clobber version #{version}" != `#{bin}/clobber --version`.strip
+      odie "Output of 'clobber --version' did not match the current version (#{version})."
     end
   end
 
